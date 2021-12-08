d1cad6737b9daf928215b034ddccf263271eab12
Bill Dealey <william.dealey@keepitsts.com>
2021-12-08 07:59:55 -0500

    adding changeInTime

--- python/sst_module_results.py
+++ python/sst_module_results.py
@@ -1,7 +1,7 @@
 import logging
 import re
 import sst_module_common as sst_common
-
+import datetime
 
 g_unofficial_results = "    ** UNOFFICIAL RESULTS **"
 
@@ -10,6 +10,30 @@ g_file_name_prefix = "event_"
 g_file_name_suffix = "RESULTS"
 g_file_name_awards = "AWARDS"
 g_crawler_name_prefix = "x_crawler_results"
+
+#####################################################################################
+### If we can display colors on wirecast
+### this will define our color pallet
+#####################################################################################
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ANSI_RESET = "\u001B[0m";
+    ANSI_BLACK = "\u001B[30m";
+    ANSI_RED = "\u001B[31m";
+    ANSI_GREEN = "\u001B[32m";
+    ANSI_YELLOW = "\u001B[33m";
+    ANSI_BLUE = "\u001B[34m";
+    ANSI_PURPLE = "\u001B[35m";
+    ANSI_CYAN = "\u001B[36m";
+    ANSI_WHITE = "\u001B[37m";
 
 
 #####################################################################################
@@ -64,13 +88,21 @@ def process_result( meet_report_filename: str,
         'relay_long':         "           Team               Relay  Seed   Finals  Pts",        
         'relay_short':       "   Team Relay Seed    Finals   Pts",    
     }
+    # result_header_dict = {
+    #     'individual_long':   "Name                    Yr School                 Seed Time  Finals Time            ",
+    #     'individual_short':  "        Name                  Sch  Yr    Seed    Finals      ",
+    #     'diving_long':       "Name                    Yr School                           Finals Score           ",
+    #     'diving_short':      "        Name                 School Yr   Seed     Final     ",
+    #     'relay_long':         "           Team                  Relay  Seed   Finals     ",        
+    #     'relay_short':       "   Team       Relay Seed Time  Finals Time       ",    
+    # }
     result_header_dict = {
-        'individual_long':   "Name                    Yr School                 Seed Time  Finals Time            ",
-        'individual_short':  "        Name                  Sch  Yr    Seed    Finals      ",
-        'diving_long':       "Name                    Yr School                           Finals Score           ",
+        'individual_long':   "Name                    Yr School                 Seed Time  Finals Time   Change   ",
+        'individual_short':  "        Name                  Sch  Yr    Seed    Finals  Change    ",
+        'diving_long':       "Name                    Yr School                           Finals Score      ",
         'diving_short':      "        Name                 School Yr   Seed     Final     ",
-        'relay_long':         "           Team                  Relay  Seed   Finals     ",        
-        'relay_short':       "   Team       Relay Seed Time  Finals Time       ",    
+        'relay_long':         "           Team                  Relay  Seed   Finals Change",        
+        'relay_short':       "   Team       Relay Seed Time  Finals Time  Change",    
     }
     ## Define local variables
     event_num = 0
@@ -217,6 +249,9 @@ def process_result( meet_report_filename: str,
                     if placeline_points == "":
                         placeline_points = "-"
 
+                    ## Get formatted string of positive/negative chanage in tie
+                    changeInTime = computeSeedFinalTimeDiff( placeline_seedtime, placeline_finaltime )
+
                     logging.debug(f"RESULTS: place {placeline_place}: name {placeline_name_last_first}: grade {placeline_grade}: sch {placeline_school_long}: seed {placeline_seedtime}: final {placeline_finaltime}: points {placeline_points}:")
                     ## If we want to use Shortened School Names, run the lookup
                     ## The length of the school name in the MM report varies by event type
@@ -235,11 +270,13 @@ def process_result( meet_report_filename: str,
                     # output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_grade:>2}{q} {q}{placeline_school_long:<25}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{placeline_points:>2}{q}"
 
                     points_str = f"{q}{placeline_points:>4}{q}" if champsionship_result_header_dict else ""
-                    output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_grade:>2}{q} {q}{full_team_name:<25}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                    #output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_grade:>2}{q} {q}{full_team_name:<25}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                    output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_grade:>2}{q} {q}{full_team_name:<25}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{changeInTime:>8}{q} {points_str}" 
                     
                     if shorten_school_names_individual:
                         #output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_school_short:<4}{q} {q}{placeline_grade:>2}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{placeline_points:>2}{q}"
-                        output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_school_short:<4}{q} {q}{placeline_grade:>2}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q}{points_str}"
+                        #output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_school_short:<4}{q} {q}{placeline_grade:>2}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q}{points_str}"
+                        output_str = f"{q}{placeline_place:>3}{q} {q}{result_name:<25}{q} {q}{placeline_school_short:<4}{q} {q}{placeline_grade:>2}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{changeInTime:>8}{q}  {points_str}"
                     
                     output_list.append(('PLACE', output_str))
                     crawler_str += gen_result_crawler_ind( placeline_place, result_name, placeline_grade,placeline_school_short, placeline_school_long, placeline_seedtime, placeline_finaltime, placeline_points )
@@ -264,6 +301,9 @@ def process_result( meet_report_filename: str,
                     if placeline_points == "":
                         placeline_points = "-"
 
+                    ## Get formatted string of positive/negative chanage in tie
+                    changeInTime = computeSeedFinalTimeDiff( placeline_seedtime, placeline_finaltime )
+
                     #####################################################################################
                     ## RESULTS: Replace long school name with short name for RELAY events
                     #####################################################################################
@@ -277,13 +317,15 @@ def process_result( meet_report_filename: str,
 
                     if shorten_school_names_relays:                        
                         #output_str = f" {q}{placeline_place:>3}{q} {q}{placeline_sch_short:<4}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{placeline_points:>2}{q}"
-                        output_str = f" {q}{placeline_place:>3}{q} {q}{placeline_sch_short:<4}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                        #output_str = f" {q}{placeline_place:>3}{q} {q}{placeline_sch_short:<4}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                        output_str = f" {q}{placeline_place:>3}{q} {q}{placeline_sch_short:<4}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{changeInTime:>8}{q} {points_str}"
                     else:
                         #full_team_name = placeline_sch_long
                         full_team_name = sst_common.find_proper_team_name( placeline_sch_long )
 
                         #output_str = f" {q}{placeline_place:>3}{q} {q}{placeline_sch_long:<25}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{placeline_points:>2}{q}"
-                        output_str = f" {q}{placeline_place:>3}{q} {q}{full_team_name:<25}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                        #output_str = f" {q}{placeline_place:>3}{q} {q}{full_team_name:<25}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {points_str}"
+                        output_str = f" {q}{placeline_place:>3}{q} {q}{full_team_name:<25}{q} {q}{placeline_relay}{q} {q}{placeline_seedtime:>8}{q} {q}{placeline_finaltime:>8}{q} {q}{changeInTime:>8}{q} {points_str}"
                     output_list.append(( "PLACE", output_str ))
 
             #####################################################################################
@@ -479,6 +521,7 @@ def create_output_file_awards(  output_dir: str,
     num_files_generated += 1
 
     return num_files_generated
+
 ####################################################################################
 ## Given an array of PROGRAM lines PER HEAT, generate the crawler file for individuals
 #####################################################################################
@@ -646,3 +689,97 @@ def generate_empty_results( output_dir:str, awards: bool ) -> int:
         num_empty_files_created += 1
 
     return num_empty_files_created
+
+
+#####################################################################################
+## convertSwimTimeToSecs
+##
+## Convert a swim time in string format to a python datetime object.
+## Valid formats are:
+## m:ss.hs  -- minute:seconds:hundreths_secs
+## ss.hs    -- seconds:hundreths_secs
+##
+## Exhibition (x) indicators are removed
+##
+## Other possible strings are ignored, such as:
+##  DQ, NT, NP, DNF
+#####################################################################################
+def convertSwimTimeToSecs( swimTimeStr: str ):
+    ## We can have either SS.hh or MM:SS.hh
+    swimTimeFormat = "%S.%f"
+    if ":" in swimTimeStr:
+        swimTimeFormat = "%M:%S.%f"
+
+    ## See if we can convert this string to a date, if its in proper format
+    ## Otherwise return None
+    try:
+        date_time_obj = datetime.datetime.strptime( swimTimeStr, swimTimeFormat )
+        return date_time_obj
+    except Exception as e:
+        ## Not a proper date format
+        return None
+
+#####################################################################################
+## computeSeedFinalTimeDiff
+##
+## Given a SEED time and FINALS time in string format see if we can
+## determine the change in time.
+## a negative number is an improvement in time (faster time)
+## a positive number is an slower time
+#####################################################################################
+def computeSeedFinalTimeDiff( seedTimeStr: str, finalTimeStr: str):
+    ## Remove xX for exhibition times
+    if 'X' in finalTimeStr:
+        finalTimeStr = finalTimeStr.replace('X','')
+    if 'x' in finalTimeStr:
+        finalTimeStr = finalTimeStr.replace('x','')
+
+    ## Convert dateformat in string format to datetime object
+    seedTimeDate = convertSwimTimeToSecs( seedTimeStr )
+    finalTimeDate = convertSwimTimeToSecs( finalTimeStr )
+
+    ## If both times are valid dates, continue.
+    seedTimeMS = 0
+    if not (seedTimeDate == None or finalTimeDate == None):
+        ## Add a + or - sign for increse/decrease in time
+        plusMinusStr = ""
+        changeTimeColorBegin = ""
+        changeTimeColorEnd = ""
+        if seedTimeDate > finalTimeDate:
+            timeDiff = seedTimeDate - finalTimeDate
+            plusMinusStr = "-"
+            changeTimeColorBegin = bcolors.ANSI_GREEN
+            changeTimeColorEnd =  bcolors.ANSI_RESET
+        else:
+            timeDiff = finalTimeDate - seedTimeDate
+            plusMinusStr = "+"
+            changeTimeColorBegin =  bcolors.ANSI_RED
+            changeTimeColorEnd =  bcolors.ANSI_RESET
+
+
+        ## is seed time a coaches entered time?
+        ## If seemdTimeMS == 00, then don't display change/improvement and assume its coaches time
+        seedTimeMS = seedTimeDate.microsecond
+
+        # total_secs includes hundreths
+        total_secs = timeDiff.total_seconds()
+
+        ## Convert 130 seconds to 2 minutes 10 seconds
+        ## Converts seconds to minutes (then subtract those seconds)
+        total_min = (total_secs//60)%60
+        adjusted_seconds = round(total_secs - (total_min*60),2)
+
+    ## if seedTimeMS == 0 then we assume this is a coaches time and don't display a change/improvement
+    if seedTimeMS > 0:
+        if total_min > 0:
+            returnStr =  f"{changeTimeColorBegin}{plusMinusStr}{total_min:.0f}:{adjusted_seconds:05.2f}{changeTimeColorEnd}"
+            #print(f"totalsecs: {seedTimeStr} -> {finalTimeStr} = {plusMinusStr}{total_secs}:  {plusMinusStr}{total_min:.0f}:{adjusted_seconds:05.2f}")
+        else:
+            returnStr =  f"{changeTimeColorBegin}{plusMinusStr}{adjusted_seconds:05.2f}{changeTimeColorEnd}"
+            #print(f"totalsecs: {seedTimeStr} -> {finalTimeStr} = {plusMinusStr}{total_secs}:  {plusMinusStr}{adjusted_seconds:05.2f}")
+    else:
+        returnStr =  f"------"
+        #print(f"totalsecs: {seedTimeStr} -> {finalTimeStr} = ------")
+    
+    logging.debug(f"computeSeedFinalTimeDiff: st: {seedTimeMS} s: '{seedTimeStr}'' f: '{finalTimeStr}'' o: {returnStr}")
+    return returnStr 
